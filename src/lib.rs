3ea5d5f5def0cf48db2063e022bd268d6cdf0cf4
Daniel Phillips <daniel@loopholelabs.io>
2023-05-03 13:19:21 -0500

    Add updates to Process per new FS

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ extern crate core;
 
 mod filesystem;
 mod process;
+mod init;
 
 pub use filesystem::{FileSystem, File};
 pub use process::Process;
--- src/process.rs
+++ src/process.rs
@@ -1,11 +1,11 @@
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
-use std::io::{Read, Write, Seek, SeekFrom};
+use std::io::{Read, Write};
 use std::path::PathBuf;
 use crate::filesystem::*;
 
 pub struct Process {
-    open_files: HashMap<FileDescriptor, OpenFile>,
+    open_files: HashMap<FileDescriptor, Arc<OpenFile>>,
     fds: FileDescriptor,
 }
 
@@ -17,52 +17,36 @@ impl Process {
         }
     }
 
-    pub fn open(&mut self, fs: &mut FileSystem, inode: Inode) -> Result<FileDescriptor, ()> {
+    pub fn open(&mut self, fs: &mut FileSystem, path: &PathBuf) -> Result<FileDescriptor, ()> {
+        let inode = fs.lookup_inode(path).ok_or(())?;
         for (&fd, open_file) in self.open_files.iter() {
-            if open_file.file.inode == inode {
+            if open_file.file.inode.number == inode.number {
                 // File is already open, return the existing FileDescriptor
                 return Ok(fd);
             }
         }
 
-        // If the file is not open, check if it exists in the filesystem
-        if let Some(file) = fs.files.get(&inode) {
-            let fd = self.fds;
-            self.open_files.insert(
-                fd,
-                OpenFile {
-                    file: Arc::clone(file),
-                    position: 0,
-                },
-            );
-            self.fds += 1;
-            return Ok(fd);
-        }
-
-        // If the inode is not provided or the file doesn't exist, create a new file
-        let new_inode = fs.create_file(vec![]);
-        let new_file = fs.files.get(&new_inode).ok_or(())?;
-        let new_fd = self.fds;
+        let file = fs.files.get(&inode).ok_or(())?;
+        let fd = self.fds;
         self.open_files.insert(
-            new_fd,
-            OpenFile {
-                file: Arc::clone(new_file),
+            fd,
+            Arc::new(OpenFile {
+                file: Arc::clone(file),
                 position: 0,
-            },
+            }),
         );
         self.fds += 1;
-        Ok(new_fd)
-}
-
+        Ok(fd)
+    }
 
     pub fn read(&mut self, fd: FileDescriptor, buf: &mut [u8]) -> Result<usize, ()> {
-        let open_file = self.open_files.get_mut(&fd).ok_or(())?;
+        let open_file = self.open_files.get(&fd).ok_or(())?;
         open_file.read(buf).map_err(|_| ())
     }
 
     pub fn write(&mut self, fd: FileDescriptor, buf: &[u8]) -> Result<usize, ()> {
-       let open_file = self.open_files.get_mut(&fd).ok_or(())?;
-       open_file.write(buf).map_err(|_| ())
+        let open_file = self.open_files.get(&fd).ok_or(())?;
+        open_file.write(buf).map_err(|_| ())
     }
 
     pub fn close(&mut self, fd: FileDescriptor) -> Result<i32, i32> {
